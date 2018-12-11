34c3d679c0ee5d0f0abe5bc9893821a8e0e3d4c6
Tan NG <leductan.nguyen@gmail.com>
2018-12-11 02:29:42 +0100

    _ADD: fan on indicator _ADD: local lib _REFACTORING

--- src/automatic_fan_control.ino
+++ src/automatic_fan_control.ino
@@ -1,3 +1,7 @@
+/**
+ * 3DRaion 2018
+ */
+
 #include <SPI.h>
 #include <Wire.h>
 
@@ -15,37 +19,32 @@ Adafruit_SSD1306 display(OLED_RESET);
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
 
-#define DHT11_PIN 2     // DHT 22  (AM2302) - what pin we're connected to
+#define DHT11_PIN 2       // DHT 22  (AM2302) - what pin we're connected to
+#define TEMP_THRESHOLD 28 // defines at which temperature should we trigger the fan
+#define TEXT_SIZE 2       // define text size
 
 // DHT constant
 dht DHT;
 // Relay pin
 constexpr uint8_t relay = 3;
 
-/* Buzzer est un programme qui joue la gamme musicale */
-#define buzzer 4    // affectation des broches
-int frequence[] = {262, 294, 330, 349, 370, 392, 440, 494};      // tableau de fréquence des notes
+// Variables
+int16_t humidity; //Stores humidity value
+int16_t temp;     //Stores temperature value
 
-//Variables
-int16_t humidity;  //Stores humidity value
-int16_t temp; //Stores temperature value
-
-// times to count for relay to be switched
-#define MAXCOUNT 5;
-uint8_t count = MAXCOUNT;
-
-void setup()   {
+void setup()
+{
   // Relay Pin Configuration
   pinMode(relay, OUTPUT);
   // Make sure the fan is off
   digitalWrite(relay, LOW);
-  
-  //Serial.begin(9600);
+
+  Serial.begin(9600);
+  Serial.println("3DRaion Automatic Fan Control");
 
   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
-  // init done
-  
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // initialize with the I2C addr 0x3C (for the 128x64)
+
   // Show image buffer on the display hardware.
   // Since the buffer is intialized with an Adafruit splashscreen
   // internally, this will display the splashscreen.
@@ -53,7 +52,8 @@ void setup()   {
   delay(500);
 }
 
-void loop() {
+void loop()
+{
   // Read data and store it to variables hum and temp
   int chk = DHT.read11(DHT11_PIN);
   humidity = DHT.humidity;
@@ -61,81 +61,52 @@ void loop() {
 
   // Print temp and humidity values to display
   displayTempAndHumidity(temp, humidity);
-  if (temp >= 28) {
+  if (temp >= TEMP_THRESHOLD)
+  {
     digitalWrite(relay, HIGH);
-    tone(buzzer, 370, 1000);
-  } else {
-    digitalWrite(relay, LOW);
+    displayFanOnIndicator(true);
   }
-  tone(buzzer, 500, 500);
+  else
+  {
+    digitalWrite(relay, LOW);
+    // displayFanOnIndicator(false);
+  }
   delay(2000);
 }
 
-void displayTempAndHumidity(int16_t t, int16_t h) {
+void displayTempAndHumidity(int16_t t, int16_t h)
+{
   display.clearDisplay();
-  display.setTextSize(2);
+  display.setTextSize(TEXT_SIZE);
   display.setTextColor(WHITE);
-  display.setCursor(10,0);
+  display.setCursor(10, 0);
   display.println(" 3DRAION ");
-  display.setTextSize(2);
+  display.setTextSize(TEXT_SIZE);
   display.setTextColor(WHITE);
-  display.setCursor(5,20);
+  display.setCursor(5, 20);
   display.print("T : ");
   display.print(t, DEC);
   display.print(" C");
-  display.setCursor(5,40);
+  display.setCursor(5, 40);
   display.print("H : ");
   display.print(h, DEC);
   display.print(" %");
   display.display();
 }
 
-void switchRelay(uint8_t value) {
-  if (value == HIGH && count == 0) {
-    digitalWrite(relay, LOW);
-    count = MAXCOUNT;
-  } else if (value == LOW && count == 0) {
-    digitalWrite(relay, HIGH);
-    count = MAXCOUNT;
+/**
+ * Display a line at the end of the screen to indicate the fan is ON
+ */
+void displayFanOnIndicator(bool on)
+{
+  if (on)
+  {
+    display.drawLine(0, 63, 128, 63, WHITE);
+    display.display();
   }
-  count--;
-}
-
-void testdrawchar(void) {
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
-
-  for (uint8_t i=0; i < 168; i++) {
-    if (i == '\n') continue;
-    display.write(i);
-    if ((i > 0) && (i % 21 == 0))
-      display.println();
-  }    
-  display.display();
-  delay(1);
-}
-
-void testscrolltext(void) {
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.setCursor(10,0);
-  display.clearDisplay();
-  display.println("scroll");
-  display.display();
-  delay(1);
- 
-  display.startscrollright(0x00, 0x0F);
-  delay(2000);
-  display.stopscroll();
-  delay(1000);
-  display.startscrollleft(0x00, 0x0F);
-  delay(2000);
-  display.stopscroll();
-  delay(1000);    
-  display.startscrolldiagright(0x00, 0x07);
-  delay(2000);
-  display.startscrolldiagleft(0x00, 0x07);
-  delay(2000);
-  display.stopscroll();
+  else
+  {
+    display.drawLine(0, 63, 128, 63, BLACK);
+    display.display();
+  }
 }
